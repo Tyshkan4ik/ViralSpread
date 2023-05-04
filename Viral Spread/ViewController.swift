8193312c21fb30a2a11e07c8d5c0727568c98264
Tyshkan4ik <001vat@gmail.com>
2023-05-04 22:38:58 +0300

    project start

---
+++ Viral Spread/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Viral Spread
+//
+//  Created by Виталий Троицкий on 04.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
